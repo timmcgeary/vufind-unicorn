4fb4914688b8ae41878225d7804de09041dfa9c6
geoffs <geoffs@nipissingu.ca>
2011-03-30 03:53:44 +0000

    Modified get_holdings to add sanity check for incoming IDs and increase efficiency of the selitem call

--- driver/sirsi_side/driver.pl
+++ driver/sirsi_side/driver.pl
@@ -5,6 +5,7 @@
 # updated 20 May 2008
 # 
 # modified by Dan Wells, Hekman Library, July 2009
+# modified by Geoff Sinclair, Nipissing University, March 2011
 # 
 # Arguments:
 #   search - type of search ('single' or 'multiple')
@@ -34,37 +35,59 @@ my $searchType = $query->param('search');
 print $query->header('text/html'); # type is left as 'html' for easy testing from a browser
 
 if ($searchType eq "single") {
-    print get_holdings($query->param('id'));
+   print get_holdings($query->param('id'));
 } elsif ($searchType eq "multiple") {
-# TODO(?): you could pass all IDs at once to get_holding for selitem efficiency reasons, but you would need to account for invalid item ids some other way (one way would be to pass through the keys, check for missing lines based on key, then fill in dummy lines as needed)
-    my $holdings = '';
-    foreach my $id (split(/\|/, $query->param('ids'))) {
-        $holdings .= get_holdings($id) if ($id ne "");
-        $holdings .= "\n";
-    }
-    chop $holdings;
-    print $holdings;
+   print get_holdings($query->param('ids'));
+} else {
+   print 'Invalid request: Missing searchType parameter';
 }
 
 sub get_holdings {
-    my $catkey = shift;
-    my $holdings = '';
-    my $api_results = 0;
-    open (API, "echo '$catkey' | selitem -iC -oCmrcy 2>/dev/null | selcallnum -iC -oCDS 2>/dev/null |");
-    while (<API>) {
-        $api_results = 1;
-        chomp;
-# check for charges, get dates
-        if (substr($_, -3) eq "|1|") {
-            $_ = `echo '$_' | selcharge -iC -oCSd 2>/dev/null`;
-        } else {
-            $_ .= "0|\n";
-        }
-        $holdings .= $_;
-    }
-    close API;
-    if (!$api_results) {
-        $holdings .= "1234|Not available|Not available|0|1|0|\n";
-    }
-    return $holdings;
+   my $ids = shift;
+   my $id;
+   my $catkey;
+   my %item_holdings;
+   my $holdings = '';
+   my $item;
+   my @ids = split(/\|/,$ids);
+   my @untainted_ids;
+
+   # sanity check for the IDs. May need fine tuning!!
+   foreach $id (@ids) {
+       if ($id =~ /^(\w{1}[\w-.]*)$/) {
+           $catkey = $1;
+          $catkey =~ s/^u//;
+           push @untainted_ids, $catkey;
+       } else {
+           # catkey failed sanity check
+           exit;
+       }
+   }
+
+   my $untainted_catkeys = '\n' . join('|\n',@untainted_ids);
+   open (API, "echo -e '$untainted_catkeys' | selitem -iC -oCmrcy 2>/dev/null | selcallnum -iC -oCDS 2>/dev/null |");
+  while (<API>) {
+      $item = $_;
+       chomp($item);
+       my $item_id = $item;
+       $item_id =~ s/\|.*//;
+       # check for charges, get dates
+       if (substr($item, -3) eq "|1|") {
+           $item = `echo '$item' | selcharge -iC -oCSd 2>/dev/null`;
+       } else {
+           $item .= "0|\n";
+       }
+       $item_holdings{$item_id} .= $item;
+   }
+  close API;
+
+   foreach $id (@untainted_ids) {
+       if ($item_holdings{$id}) {
+           $holdings .= $item_holdings{$id} . "\n";
+       } else {
+           $holdings .= "2345|Not available|Not available|0|1|0|\n\n";
+       }
+   }
+   chop $holdings;
+   return $holdings;
 }
